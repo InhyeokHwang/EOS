a033584dc68f8cddff6fbc352de7be386de74c3d
Inhyeok <hwai566877@naver.com>
2023-03-16 14:20:28 +0900

    second commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:eos_chatting/widget/clock_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFffffff),
+      appBar: AppBar(
+        title: Text('EOS 시계 앱'),
+        leading: Icon(Icons.access_time_filled),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Spacer(
+                ),
+                Image.asset("assets/images/eos_logo.png", width: 280, height: 280),
+                ClockWidget(
+
+                ),
+                Spacer(
+                ),
+              ]
+          )
+        )
+      )
+    );
+  }
+}
